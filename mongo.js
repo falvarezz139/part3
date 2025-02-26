7f9774b080b00092cdca7e2237811aca5f19d7c6
Fernando Alvarez <falvarez@lauraotero.com>
2025-02-26 13:09:24 +0100

    ejercicio 3.12

--- mongo.js
+++ mongo.js
@@ -1,11 +1,8 @@
 const mongoose = require("mongoose");
 
-if (process.argv.length < 3) {
-  console.log("give password as argument");
-  process.exit(1);
-}
-
 const password = process.argv[2];
+const name = process.argv[3];
+const number = process.argv[4];
 
 const url = `mongodb+srv://falvarezz139:${password}@cluster0.vj5ig.mongodb.net/PhoneBook?retryWrites=true&w=majority`;
 
@@ -15,42 +12,40 @@ mongoose
   .connect(url)
   .then(() => {
     console.log("Connected to MongoDB Atlas");
+
+    const personSchema = new mongoose.Schema({
+      name: String,
+      number: String,
+    });
+
+    const Person = mongoose.model("Person", personSchema);
+
+    if (name && number) {
+      const person = new Person({
+        name,
+        number,
+      });
+
+      person
+        .save()
+        .then(() => {
+          console.log(`added ${name} number ${number} to phonebook`);
+          mongoose.connection.close();
+        })
+        .catch((err) => {
+          console.error("Error saving person:", err.message);
+          mongoose.connection.close();
+        });
+    } else {
+      Person.find({}).then((result) => {
+        console.log("phonebook:");
+        result.forEach((person) => {
+          console.log(`${person.name} ${person.number}`);
+        });
+        mongoose.connection.close();
+      });
+    }
   })
   .catch((err) => {
     console.error("Error connecting to MongoDB:", err.message);
-  });
-
-const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
-  important: Boolean,
-});
-
-const Person = mongoose.model("Person", personSchema);
-
-const person = new Person({
-  name: "John Doe",
-  number: "123-456-7890",
-  important: true,
-});
-
-Person.find({})
-  .then((result) => {
-    console.log("All persons:");
-    result.forEach((person) => {
-      console.log(person);
-    });
-
-    return Person.find({ important: true });
-  })
-  .then((importantPersons) => {
-    console.log("Persons with important: true:");
-    importantPersons.forEach((person) => {
-      console.log(person);
-    });
-
-    mongoose.connection.close();
-  })
-  .catch((err) => {
-    console.error("Error:", err.message);
   });
