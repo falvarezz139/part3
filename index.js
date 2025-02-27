62dcfdd12dd76aa892f3d72b2180d06a8f3e741a
Fernando Alvarez <falvarez@lauraotero.com>
2025-02-27 13:28:48 +0100

    Ejercicio 3.14

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ console.log("connecting to", url);
 
 mongoose.set("strictQuery", false);
 
-mongoose
+mongoose //conexion a mongo
   .connect(url)
   .then(() => {
     console.log("connected to MongoDB");
@@ -28,13 +28,12 @@ morgan.token("body", (req) => {
   return "";
 });
 
-app.use(express.json());
+app.use(express.json()); //se usa express
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
 );
 app.use(cors());
 app.use(express.static("dist"));
-
 app.get("/api/persons", (request, response) => {
   Person.find({}).then((persons) => {
     response.json(persons);
@@ -56,6 +55,17 @@ app.post("/api/persons", (request, response) => {
   person.save().then((savedPerson) => {
     response.json(savedPerson);
   });
+});
+
+app.delete("/api/persons/:id", async (req, res) => {
+  const { id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return res.status(400).json({ error: "Invalid ID format" });
+
+  const deletedPerson = await Person.findByIdAndDelete(id);
+  deletedPerson
+    ? res.status(204).end()
+    : res.status(404).json({ error: "Person not found" });
 });
 
 const PORT = process.env.PORT || 3001;
