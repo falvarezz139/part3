5cd433e5a7d5618cf9aa4f9426c6ca5daf07dfb1
Fernando Alvarez <falvarez@lauraotero.com>
2025-02-27 11:11:19 +0100

    antes ejerciucio 3.13

--- index.js
+++ index.js
@@ -1,74 +1,68 @@
+require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+const mongoose = require("mongoose");
+const Person = require("./models/person");
 
-const app = express();
+if (process.argv.length < 3) {
+  console.log("give password as argument");
+  process.exit(1);
+}
 
-app.use(express.json());
+const password = process.argv[2];
 
-morgan.token("body", (req) => {
-  return req.method === "POST" ? JSON.stringify(req.body) : ""; //convierte la peticion en un string
+const url = `mongodb+srv://falvarezz139:${password}@cluster0.vj5ig.mongodb.net/PhoneBook?retryWrites=true&w=majority`;
+
+mongoose.set("strictQuery", false);
+mongoose.connect(url);
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
 });
 
+personSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString(); // Convierte _id en string
+    delete returnedObject._id; // borra _id
+    delete returnedObject.__v; // borra __v
+  },
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+const app = express();
+app.use(express.json());
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
 );
 app.use(cors());
-
 app.use(express.static("dist"));
 
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
-
-app.get("/api/persons", (request, response) => {
-  //trae los datos
-  response.json(persons);
+app.get("/api/person/:id", (request, response) => {
+  Person.findById(request.params.id).then((person) => {
+    response.json(person);
+  });
 });
 
 app.post("/api/persons", (request, response) => {
-  //agrega persona nueva
-  const { name, number } = request.body;
+  const body = request.body;
 
-  if (!name || !number) {
+  if (!body.name || !body.number) {
     return response.status(400).json({ error: "Name and number are required" });
   }
 
-  if (persons.some((p) => p.name === name)) {
-    return response.status(400).json({ error: "Name must be unique" });
-  }
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+  });
 
-  const newPerson = {
-    //crea un nuevo objeto
-    id: Math.floor(Math.random() * 1000000),
-    name,
-    number,
-  };
-
-  persons = persons.concat(newPerson);
-  response.json(newPerson);
+  person.save().then((savedPerson) => {
+    response.json(savedPerson);
+  });
 });
-
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
--- mongo.js
+++ mongo.js
@@ -1,5 +1,10 @@
 const mongoose = require("mongoose");
 
+if (process.argv.length < 3) {
+  console.log("give password as argument");
+  process.exit(1);
+}
+
 const password = process.argv[2];
 const name = process.argv[3];
 const number = process.argv[4];
