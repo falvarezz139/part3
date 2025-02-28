d49b5df8fc354cb04ea26a28a34bef06b16a769a
Fernando Alvarez <falvarez@lauraotero.com>
2025-02-28 09:41:29 +0100

    prueba 3

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ console.log("connecting to", url);
 
 mongoose.set("strictQuery", false);
 
-mongoose //conexion a mongo
+mongoose
   .connect(url)
   .then(() => {
     console.log("connected to MongoDB");
@@ -28,25 +28,17 @@ morgan.token("body", (req) => {
   return "";
 });
 
-app.use(express.json()); //se usa express
+app.use(express.json());
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
 );
 app.use(cors());
 app.use(express.static("dist"));
+
 app.get("/api/persons", (request, response) => {
-  Person.findById(request.params.id)
-    .then((person) => {
-      if (person) {
-        response.json(person);
-      } else {
-        response.status(404).end();
-      }
-    })
-    .catch((error) => {
-      console.log(error);
-      response.status(500).end();
-    });
+  Person.find({}).then((persons) => {
+    response.json(persons);
+  });
 });
 
 app.post("/api/persons", (request, response) => {
@@ -64,20 +56,4 @@ app.post("/api/persons", (request, response) => {
   person.save().then((savedPerson) => {
     response.json(savedPerson);
   });
-});
-
-app.delete("/api/persons/:id", async (req, res) => {
-  const { id } = req.params;
-  if (!mongoose.Types.ObjectId.isValid(id))
-    return res.status(400).json({ error: "Invalid ID format" });
-
-  const deletedPerson = await Person.findByIdAndDelete(id);
-  deletedPerson
-    ? res.status(204).end()
-    : res.status(404).json({ error: "Person not found" });
-});
-
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
 });
