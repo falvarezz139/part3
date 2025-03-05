16142c6276bacda0609bd56c93ccb570fdd0cfb3
Fernando Alvarez <falvarez@lauraotero.com>
2025-03-05 11:07:13 +0100

    ejercicio 4.4

--- tests/list_helper.test.js
+++ tests/list_helper.test.js
@@ -1,10 +1,42 @@
-const { test } = require("node:test");
-const assert = require("node:assert");
+// tests/list_helper_test.js
+const assert = require("assert");
 const listHelper = require("../utils/list_helper");
 
-test("dummy returns one", () => {
-  const blogs = [];
+const listWithOneBlog = [
+  {
+    _id: "5a422aa71b54a676234d17f8",
+    title: "Go To Statement Considered Harmful",
+    author: "Edsger W. Dijkstra",
+    url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
+    likes: 5,
+    __v: 0,
+  },
+];
 
-  const result = listHelper.dummy(blogs);
-  assert.strictEqual(result, 1);
-});
+// Test para la función totalLikes
+const result1 = listHelper.totalLikes(listWithOneBlog);
+assert.strictEqual(
+  result1,
+  5,
+  "Total likes should be 5 when there is only one blog"
+);
+
+const result2 = listHelper.totalLikes([
+  { likes: 3 },
+  { likes: 4 },
+  { likes: 5 },
+]);
+assert.strictEqual(
+  result2,
+  12,
+  "Total likes should be 12 when there are multiple blogs"
+);
+
+const result3 = listHelper.totalLikes([]);
+assert.strictEqual(
+  result3,
+  0,
+  "Total likes should be 0 when the list is empty"
+);
+
+console.log("All tests passed!");
